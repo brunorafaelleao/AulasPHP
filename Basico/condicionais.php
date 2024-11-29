f8c1f7748f3adbe21931e98d6b9ea86d31b847d8
Bruno Leão <bruno.melosh@gmail.com>
2024-11-29 15:40:20 -0400

    condicionais e operacao matematica

---
+++ Basico/condicionais.php
@@ -0,0 +1,10 @@
+<?php
+$var1 = 10;
+$var2 = 20;
+
+if($var1 == $var2){
+    echo 'As variáveis são iguais';
+}else{
+  echo 'As variáveis são diferentes';
+}
+?>
---
+++ Basico/operacoes.php
@@ -0,0 +1,16 @@
+<?php
+$numero = 6;
+$numero2 = 2;
+echo 'Dados os números: "'.$numero. '" e "'.$numero2.'"';
+echo '<br>';
+echo 'soma: '.$numero + $numero2;
+echo '<br>';
+echo 'subtração: '. $numero - $numero2;
+echo '<br>';
+echo 'multiplicação: '. $numero * $numero2;
+echo '<br>';
+echo 'divisão: '. $numero / $numero2;
+echo '<br>';
+echo 'resto: '. $numero % $numero2;
+
+?>
