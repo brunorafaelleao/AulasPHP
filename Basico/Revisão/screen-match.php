a4a4998ff2833b6749e96ab6c4dcc3df6ec7f412
Bruno Leão <bruno.melosh@gmail.com>
2024-12-03 16:53:22 -0400

    reivisando php basico - inicio

---
+++ Basico/Revisão/screen-match.php
@@ -0,0 +1,12 @@
+<?php
+ echo 'Bem-vindo ao Screen Match!
+';
+ 
+ $nomeFilme = "Top Gun - Maverick
+ ";
+ $anoLancamento = 2022;
+ $notaFilme = 8.8;
+ $incluidoNoPlano = true;
+
+ echo "Nome do filme: ". $nomeFilme;
+ echo 0.1+0.2;
