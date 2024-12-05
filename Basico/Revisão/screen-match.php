0f6126267d4e9809b5934b392acc4853126300e8
Bruno Leão <bruno.melosh@gmail.com>
2024-12-05 15:47:21 -0400

    expressão match

--- Basico/Revisão/screen-match.php
+++ Basico/Revisão/screen-match.php
@@ -1,13 +1,25 @@
 <?php
- echo "Bem-vindo ao Screen Match!\n";
-//  echo "\n";
+//  echo "Bem-vindo ao Screen Match!\n";
+// //  echo "\n";
  
- $nomeFilme = "Top Gun - Maverick";
- $anoLancamento = 2022;
- $notaFilme = 8.8;
- $incluidoNoPlano = true;
+//  $nomeFilme = "Top Gun - Maverick";
+//  $anoLancamento = 2022;
+//  $notaFilme = 8.8;
+//  $incluidoNoPlano = true;
 
- echo "Nome do filme: ". $nomeFilme. "\n";
-  // echo "\n";
- echo 0.1+0.2;
+//  echo "Nome do filme: ". $nomeFilme. "\n";
+//   // echo "\n";
+//  echo 0.1+0.2;
+$pessoa = $argv[1];
+
+
+$idade = match ($pessoa) {
+  "Bruno" => 35,
+  "Chiara" => 0,
+  "Karol" => 32,
+  default => "Idade desconhecida",
+ };
+ echo "A idade da pessoa é: $idade";
+
+
 ?>
