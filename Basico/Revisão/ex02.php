31cb5a7e24433fe3d660220c22fc855eaa7cbfe8
Bruno Leão <bruno.melosh@gmail.com>
2025-02-19 09:18:13 -0400

    atualizacoes de revisao

---
+++ Basico/Revisão/ex02.php
@@ -0,0 +1,36 @@
+<?php
+/*
+// 1 - Escreva um programa que exiba, na tela do usuário, todos os números ímpares de 0 à 100.
+for ($i = 0; $i <= 100; $i++) {
+  if ($i % 2 != 0) {
+    echo ("$i\n");
+  }
+}
+*/
+
+//2-Crie um programa que, a partir de altura e peso, calcule o IMC e exiba a classificação do IMC.
+/*
+$altura = 1.67;
+$peso = 79;
+$calc = $peso / ($altura * $altura);
+$imc = number_format($calc, 2);
+echo ("IMC: $imc\n");
+*/
+
+//3-Desenvolva um programa que exiba na tela uma saudação (bom dia, boa tarde ou boa noite)
+// dependendo do horário encontrado em uma variável (inteiro representando as horas).
+// Defina a variável com o horário (inteiro representando as horas)
+$hora = 15; // Exemplo: 15 representa 15:00 ou 3:00 PM
+
+// Verifique o horário e exiba a saudação apropriada
+if ($hora >= 0 && $hora < 12) {
+    echo "Bom dia\n";
+} elseif ($hora >= 12 && $hora < 18) {
+    echo "Boa tarde\n";
+} elseif ($hora >= 18 && $hora <= 23) {
+    echo "Boa noite\n";
+} else {
+    echo "Hora inválida\n";
+}
+
+?>
