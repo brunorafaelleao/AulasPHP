c7eb96df088d6a033706e23b2018933f1bbd2908
Bruno Leão <bruno.melosh@gmail.com>
2025-04-02 17:03:54 -0400

    inicio aulas revisao

---
+++ Basico/Revisão/composer-exemplo/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cf3fdcdce4522a5a7d0dfff6569c4ecd4e3c15e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cf3fdcdce4522a5a7d0dfff6569c4ecd4e3c15e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
