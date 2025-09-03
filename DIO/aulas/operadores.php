386aff9e50d7ffec7c3dd6c8c132f62da533431e
Bruno Leão <bruno.melosh@gmail.com>
2025-09-03 16:59:29 -0400

    inicio aula operadores logicos

---
+++ DIO/aulas/operadores.php
@@ -0,0 +1,18 @@
+<?php
+//E
+
+echo $a and $b;
+echo $a && $b;
+
+//OU
+echo $a or $b;
+echo $a || $b;
+
+//Ou exclusivo. Ou a ou b, mas nao os dois ao mesmo tempo
+echo $a xor $b;
+
+//negação
+echo !$a;
+
+
+?>
